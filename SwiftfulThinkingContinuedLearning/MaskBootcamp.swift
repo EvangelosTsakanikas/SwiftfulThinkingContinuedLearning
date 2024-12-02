54e8a83bc619bdf4683c1873b6dacaa1474124b6
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-12-02 23:27:41 +0100

    Added mask for 5 star view.

---
+++ SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -0,0 +1,53 @@
+//
+//  MaskBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by user268168 on 12/2/24.
+//
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    
+    @State var rating: Int = 0
+    
+    var body: some View {
+        ZStack {
+            starsView
+                .overlay(
+                    overlayView
+                        .mask(starsView)
+                )
+        }
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {                        
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundStyle(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+}
+
+#Preview {
+    MaskBootcamp()
+}
