d25713dc31ce27c0c9f26e61ddb76e8c6ea67f0c
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-12-03 23:21:08 +0100

    Finished Core Data Bootcamp with FetchRequest.

--- CoreDataBootcamp/CoreDataBootcamp/ContentView.swift
+++ CoreDataBootcamp/CoreDataBootcamp/ContentView.swift
@@ -11,38 +11,82 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
-    @FetchRequest(sortDescriptors: []) var fruits: FetchedResults<FruitEntity>
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \FruitEntity.name, ascending: true)])
+    var fruits: FetchedResults<FruitEntity>
+    
+    @State var textFieldText: String = ""
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(fruits) { fruit in
-                    Text(fruit.name ?? "")
+            VStack(spacing: 20) {
+                
+                TextField("Add fruit here...", text: $textFieldText)
+                    .font(.headline)
+                    .padding(.leading)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(.gray)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .padding(.horizontal)
+                
+                Button {
+                    addFruit()
+                } label: {
+                    Text("Submit")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(.indigo)
+                        .clipShape(.rect(cornerRadius: 10))
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .listStyle(.plain)
-            .navigationTitle("Core Data Bootcamp")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: addFruit) {
-                        Label("Add Item", systemImage: "plus")
+                .padding(.horizontal)
+
+                
+                List {
+                    ForEach(fruits) { fruit in
+                        Text(fruit.name ?? "")
+                            .onTapGesture {
+                                updateFruit(fruit: fruit)
+                            }
                     }
-                    
+                    .onDelete(perform: deleteFruit)
                 }
+                .listStyle(.plain)
             }
+            .navigationTitle("Core Data Bootcamp")
+//            .toolbar {
+//                ToolbarItem(placement: .topBarTrailing) {
+//                    Button(action: addFruit) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                    
+//                }
+//            }
         }
     }
     
     private func addFruit() {
         withAnimation {
             let newFruit = FruitEntity(context: viewContext)
-            newFruit.name = "Orange"
+            newFruit.name = textFieldText
+            saveItems()
+            textFieldText = ""
+        }
+    }
+    		
+    private func updateFruit(fruit: FruitEntity) {
+        withAnimation {
+            let currentName = fruit.name ?? ""
+            let newName = currentName + "!"
+            fruit.name = newName
+            
             saveItems()
         }
     }
     
-    private func deleteItems(offsets: IndexSet) {
+    private func deleteFruit(offsets: IndexSet) {
         withAnimation {
             guard let index = offsets.first else { return }
             let fruitEntity = fruits[index]
