77b941f210aa0c238217dbb4fffcea79a5bb7a30
Evangelos Tsakanikas <evangelostsakanikas@gmail.com>
2025-01-08 16:48:25 +0100

    Added Download JSON with escaping closure Bootcamp

---
+++ SwiftfulThinkingContinuedLearning/DownloadWithEscapingBootcamp.swift
@@ -0,0 +1,95 @@
+//
+//  DownloadWithEscapingBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by user274186 on 1/8/25.
+//
+
+import SwiftUI
+
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithEscapingViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        downloadData(from: url) { returnedData in
+            if let data = returnedData {
+                guard let newPosts = try? JSONDecoder().decode([PostModel].self, from: data) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.posts = newPosts
+                }
+            } else {
+                print("No data returned")
+            }
+        }
+        
+        //            guard error == nil else {
+        //                print("Error: \(String(describing: error))")
+        //                return
+        //            }
+        //
+        //            guard let response = response as? HTTPURLResponse else {
+        //                print("Invalid response")
+        //                return
+        //            }
+        //
+        //            guard response.statusCode >= 200 && response.statusCode < 300 else {
+        //                print("Status code should be 2xx, but is \(response.statusCode)")
+        //                return
+        //            }
+    }
+    
+    func downloadData(from url: URL, completionHandler: @escaping (_ data: Data?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300
+            else {
+                print("Error downloading data")
+                completionHandler(nil)
+                return
+            }
+            
+            completionHandler(data)
+        }
+        .resume()
+    }
+}
+
+struct DownloadWithEscapingBootcamp: View {
+    
+    @StateObject var vm = DownloadWithEscapingViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(vm.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundStyle(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
+#Preview {
+    DownloadWithEscapingBootcamp()
+}
